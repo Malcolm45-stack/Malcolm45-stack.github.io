60c6f140267736d17bff095852e92211303e5742
Malcolm45-stack <malcolm.motubatse@bluepearl.co.za>
2020-05-06 03:46:56 +0200

    updating the order of js files

--- index.php
+++ index.php
@@ -108,12 +108,7 @@
 		return false;
 	 }
 	</script>
-  <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
   
-
-  <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <!-- Custom scripts for all pages-->
   <script src="js/sb-admin-2.min.js"></script>
