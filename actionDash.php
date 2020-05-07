4d7a9e74af5d9fa532553b8bb108b703b977233a
Malcolm45-stack <malcolm.motubatse@bluepearl.co.za>
2020-05-07 02:46:11 +0200

    datatables

--- actionDash.php
+++ actionDash.php
@@ -33,7 +33,7 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <link rel ="icon" href="img/logo2.png" type="image/x-icon">
   <!-- Custom styles for this template-->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-sb-admin-2/3.3.7+1/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
   
 </head>
 
--- admin.php
+++ admin.php
@@ -28,7 +28,7 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <link rel ="icon" href="img/logo2.png" type="image/x-icon">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-sb-admin-2/3.3.7+1/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
  
 </head>
 
