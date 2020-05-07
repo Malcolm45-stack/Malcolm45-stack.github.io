da75813bdf840ff06b7763a4a4caf0087cee9235
Malcolm45-stack <malcolm.motubatse@bluepearl.co.za>
2020-05-07 22:02:20 +0200

    Update tables.php

--- tables.php
+++ tables.php
@@ -495,7 +495,11 @@
       </div>
     </div>
   </div>
-
+<script>
+$(document).ready(function() {
+    $('#dataTable').DataTable();
+} );
+</script>
   <!-- Bootstrap core JavaScript-->
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
