60f2f3a5f3078705e6336ba341e8b55b65cf3852
Malcolm45-stack <malcolm.motubatse@bluepearl.co.za>
2020-05-07 02:43:33 +0200

    datatables

--- tables.php
+++ tables.php
@@ -45,9 +45,9 @@
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <link rel ="icon" href="img/logo2.png" type="image/x-icon">
   <!-- Custom styles for this template -->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-sb-admin-2/3.3.7+1/css/sb-admin-2.min.css" rel="stylesheet">
   <!-- Custom styles for this page -->
-  <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+  <link href="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" rel="stylesheet">
 </head>
 
 <body id="page-top">
