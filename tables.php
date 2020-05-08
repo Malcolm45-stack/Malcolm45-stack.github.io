328c31c715e0b881b347fa240123609fef1c2f73
Malcolm45-stack <malcolm.motubatse@bluepearl.co.za>
2020-05-08 14:51:03 +0200

    JQuery Files

--- tables.php
+++ tables.php
@@ -502,7 +502,7 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.0/js/bootstrap.min.js.map"></script>
 
 <!-- Core plugin JavaScript-->
