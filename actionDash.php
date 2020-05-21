36c1932da08ae5235e4dc523909759535d49cb1b
Malcolm45-stack <64865490+Malcolm45-stack@users.noreply.github.com>
2020-05-21 10:59:43 +0200

    Update actionDash.php

--- actionDash.php
+++ actionDash.php
@@ -178,9 +178,11 @@
 
           <!-- Content Row -->
           <div class="row">
+            <div class="card shadow mb-4">
 			      <div class="card-body">
              <iframe width="1245" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiMmUyY2MxZjEtNmJmMS00NDU0LWJjNmEtMTJkMDFiZjljZjZjIiwidCI6IjRiMjFkZTUxLTU3YTEtNDNhYi05ODhkLTYyOTcyMjgzMmI2NSIsImMiOjl9" frameborder="0" allowFullScreen="true"></iframe>
 			      </div>
+          </div>
 		     </div>
          </div>
       </div>
