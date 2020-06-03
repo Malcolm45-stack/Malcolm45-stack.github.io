cbdec6d2d9f98049d719b20a7333ee35d9b317a4
Malcolm45-stack <64865490+Malcolm45-stack@users.noreply.github.com>
2020-06-03 12:01:23 +0200

    Update notification.php

--- notification.php
+++ notification.php
@@ -1,76 +1,48 @@
 <?php
 include "config.php";
 
+if(!isset($_SESSION))
+{
+    session_start();
+}
+
 if(isset($_POST["view"])){
 
-$result = mysqli_query($con,"SELECT * FROM employees ORDER BY ID DESC LIMIT 5");
+  if($_POST["view"] != ''){
+    $update_q = mysqli_query($con,"UPDATE employees SET NOT_ID = 1 WHERE NOT_ID = 0");
+  }
+
+$result = mysqli_query($con,"SELECT * FROM employees WHERE NOT_ID = 0 ORDER BY ID DESC LIMIT 5");
 $output = '';
-if(mysqli_num_rows($result>0)){
+date_default_timezone_set('Africa/Johannesburg');
+
+if(mysqli_num_rows($result)>0){
   while($row=mysqli_fetch_array($result)){
-    $output .= '<li class="nav-item dropdown no-arrow mx-1">
-                  <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-bell fa-fw"></i>
-                      <!-- Counter - Alerts -->
-                      <span class="badge badge-danger badge-counter">3+</span>
-                  </a>
-                      <!-- Dropdown - Alerts -->
-                  <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
-                    <h6 class="dropdown-header">
-                       Alerts Center
-                    </h6>
-                    <a class="dropdown-item d-flex align-items-center" href="#">
-                    <div class="mr-3">
-                        <div class="icon-circle bg-primary">
-                          <i class="fas fa-file-alt text-white"></i>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="small text-gray-500">'.date("mmm/dd/yyyy").' '. date("h:i:sa").'</div>
-                        <span class="font-weight-bold">'.row["PROJECT"].'</span>
-                    </div>
-                    </a>
-                  </div>
-                </li>';
+            $output .= '<li>
+                         <a class="dropdown-item d-flex align-items-center" href="#">
+                         <span class="font-weight-bold">'.$row["PROJECT"].'</span>
+                         <div class="small text-gray-500">'.date("mmm/dd/yyyy").' '. date("h:i:sa").'</div>
+                         </a>
+                        </li>';
               }
 
+            } else{
+            $output .= '<li>
+                            <span class="font-weight-bold"> No Notifications Found</span>
+                        </li>';
+           }
 
-} else{
-  $output .= '<li class="nav-item dropdown no-arrow mx-1">
-                <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="fas fa-bell fa-fw"></i>
-                    <!-- Counter - Alerts -->
-                    <span class="badge badge-danger badge-counter">3+</span>
-                </a>
-                    <!-- Dropdown - Alerts -->
-                <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
-                  <h6 class="dropdown-header">
-                     Alerts Center
-                  </h6>
-                  <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="mr-3">
-                      <div class="icon-circle bg-primary">
-                        <i class="fas fa-file-alt text-white"></i>
-                      </div>
-                  </div>
-                  <div>
-                      <div class="small text-gray-500">December 12, 2019</div>
-                      <span class="font-weight-bold">No Notification Found</span>
-                  </div>
-                  </a>
-                </div>
-              </li>';
-   }
+           $result_1 = mysqli_query($con,"SELECT * FROM employees WHERE NOT_ID = 0 ");
+           $count = mysqli_num_rows($result_1);
+          // $result_2 = mysqli_query($con,"SELECT * FROM employees WHERE NOT_ID = 1 ");
+          // $count1 = mysqli_num_rows($result_2);
+           $data = array (
+             'notification'         => $output,
+             'unseen_notification'  => $count
+           );
 
-   $result_1 = mysqli_query($con,"SELECT * FROM employees WHERE ID = 0");
-   $count = mysqli_num_rows($result_1);
-   $data = array (
-     'notification'         => $output,
-     'unseen_notification'  => $count
-   );
-
-   echo json_encode($data);
-
-  }
+           echo json_encode($data);
+          }
 
 
 ?>
