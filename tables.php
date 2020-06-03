8cfea6356105cdf489d170473259ce90eaa53aaf
Malcolm45-stack <64865490+Malcolm45-stack@users.noreply.github.com>
2020-06-03 13:22:15 +0200

    Update tables.php

--- tables.php
+++ tables.php
@@ -22,8 +22,8 @@
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
 
-	 //$result = mysqli_query($con,"SELECT * FROM employees WHERE TEAM_MEMBER ='". $_SESSION['username'] ."'");
-	 $result = mysqli_query($con,"SELECT * FROM employees");
+	 $result = mysqli_query($con,"SELECT * FROM employees WHERE TEAM_MEMBER ='". $_SESSION['username'] ."'");
+	 //$result = mysqli_query($con,"SELECT * FROM employees");
 
 
 
